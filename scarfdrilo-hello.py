2dbbd1e35dcadc044529532f79b80db85469b02e
Scarfdrilo <scarfdrilo@example.com>
2026-04-06 08:45:23 +0000

    Initial commit: Scarfdrilo hello script

---
+++ scarfdrilo-hello.py
@@ -0,0 +1,24 @@
+
+import os
+
+def main():
+    crocodile_art = """
+      .--.
+     /    \
+    |  🐊 |
+     \ __ /
+       ||
+      /||\
+     / || \
+    /  ||  \
+   /___||___\
+  /__________\
+  |          |
+  \__________/
+    (o)  (o)  <-- Scarf!
+    """
+    print(crocodile_art)
+    print("Scarfdrilo está en línea 🐊")
+
+if __name__ == "__main__":
+    main()
